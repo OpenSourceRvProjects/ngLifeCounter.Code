2b08e11b1eb42ee23908705505f5db53c458415b
dev-rvdl <dev-rvdl@outlook.es>
2024-03-03 15:28:35 -0600

    Add counter validations

--- ngLifeCounter.System/ngLifeCounter.Code/ClientApp/src/app/modules/counter/add-counter/add-counter.component.ts
+++ ngLifeCounter.System/ngLifeCounter.Code/ClientApp/src/app/modules/counter/add-counter/add-counter.component.ts
@@ -13,17 +13,54 @@ import { EventService } from 'src/app/Services/Events/event.service';
 export class AddCounterComponent {
 
   newCounterEvent : INewEventCounterModel = <INewEventCounterModel>{};
+  processing : boolean = false;
+  errorMessage: string = "";
 
   constructor(private eventCounterService: EventService, private router: Router){}
 
 
   addEvent(){
+    debugger;
+
+    if (this.newCounterEvent.eventName == undefined || this.newCounterEvent.eventName.trim() == ''){
+      this.errorMessage = "El nombre de tu evento sirve de identificador, ingrésalo";
+      return;
+    }
+
+    if (this.newCounterEvent.year == undefined || this.newCounterEvent.year <= 1900 || this.newCounterEvent.year > 3900){
+      this.errorMessage = "Año inválido, ingresa un año mayor a 1900 y menor a 3900";
+      return;
+    }
+
+    if (this.newCounterEvent.month == undefined || this.newCounterEvent.month <= 0 || this.newCounterEvent.month > 12){
+      this.errorMessage = "Mes inválido";
+      return;
+    }
+
+    
+    if (this.newCounterEvent.day == undefined || this.newCounterEvent.day <= 0 || this.newCounterEvent.day > 31){
+      this.errorMessage = "Día inválido";
+      return;
+    }
+
+    if (this.newCounterEvent.hour == undefined || this.newCounterEvent.hour < 0 || this.newCounterEvent.hour > 24){
+      this.errorMessage = "Hora inválida";
+      return;
+    }
+
+    if (this.newCounterEvent.minutes == undefined || this.newCounterEvent.minutes < 0 || this.newCounterEvent.minutes > 60){
+      this.errorMessage = "Minutos inválidos";
+      return;
+    }
+
+
+    this.processing = true;
     this.eventCounterService.addEvent(this.newCounterEvent)
     .subscribe({next: ()=>{
-      debugger;
+      this.processing = false;
       this.router.navigate(["/"]);
     }, error: (err) => {
-      debugger;
+      this.processing = false;
       alert("Error!!!");
     }})
   }
