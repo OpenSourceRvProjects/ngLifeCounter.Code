df0fff6c6fbc846a3558b32b83cde6fe96f2ddbc
dev-rvdl <dev-rvdl@outlook.es>
2024-03-03 00:18:03 -0600

    Guard for add event

--- ngLifeCounter.System/ngLifeCounter.Code/ClientApp/src/app/modules/counter/counter-routing.module.ts
+++ ngLifeCounter.System/ngLifeCounter.Code/ClientApp/src/app/modules/counter/counter-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { AddCounterComponent } from "./add-counter/add-counter.component";
+import { UserGuard } from "src/app/Security/user.guard";
 
 
 const routes: Routes = [
-  { path: 'counter/add', component: AddCounterComponent, data: {name : "Agregar evento", showInNavBar : true}}
+  { path: 'counter/add', component: AddCounterComponent, canActivate: [UserGuard], data: {name : "Agregar evento", showInNavBar : true}}
 ];
 
 @NgModule({
