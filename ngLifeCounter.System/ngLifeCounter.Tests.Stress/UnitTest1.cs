087d036850cc51fb4881cc45d491bfbd05b4702a
dev-rvdl <dev-rvdl@outlook.es>
2024-03-29 23:48:46 -0600

    User simulations

--- ngLifeCounter.System/ngLifeCounter.Tests.Stress/UnitTest1.cs
+++ ngLifeCounter.System/ngLifeCounter.Tests.Stress/UnitTest1.cs
@@ -1,7 +1,9 @@
 using Microsoft.EntityFrameworkCore;
 using ngLifeCounter.Backend.Services;
 using ngLifeCounter.Data.DataAccess;
+using ngLifeCounter.Models.Account;
 using ngLifeCounter.Security.Core;
+using System.Text.Json;
 
 namespace ngLifeCounter.Tests.Stress
 {
@@ -12,7 +14,7 @@ namespace ngLifeCounter.Tests.Stress
 		[TestInitialize]
 		public void SetContexts()
 		{
-			var connectionStrings  = "workstation id=dev-NgLifeCounterDB.mssql.somee.com;packet size=4096;user id=dev-nglifecount_SQLLogin_1;pwd=nxicab5sa6;data source=dev-NgLifeCounterDB.mssql.somee.com;persist security info=False;initial catalog=dev-NgLifeCounterDB;Encrypt=False";
+			var connectionStrings = "workstation id=dev-NgLifeCounterDB.mssql.somee.com;packet size=4096;user id=dev-nglifecount_SQLLogin_1;pwd=nxicab5sa6;data source=dev-NgLifeCounterDB.mssql.somee.com;persist security info=False;initial catalog=dev-NgLifeCounterDB;Encrypt=False";
 			_dbContext = CreateDBContext(connectionStrings);
 		}
 
@@ -27,55 +29,162 @@ namespace ngLifeCounter.Tests.Stress
 		[TestMethod]
 		public void TestMethod1()
 		{
-		
-			var user = _dbContext.Users.Include(i=> i.PersonalProfiles).FirstOrDefault();
-			Random r = new Random();
-			for (var i = 0; i < 5000; i++)
-			{
-				var eventCounter = new EventCounter()
-				{
-					UserId = user.Id,
-					PersonalProfileId = user.PersonalProfiles.First().Id,
-					CreationDate = DateTime.Now,
-					EventName = "Test  " + Guid.NewGuid(),
-					StartDay = r.Next(1, 28),
-					StartMonth = r.Next(1, 12),
-					StartYear = r.Next(1990, 2030),
-					Hour = r.Next(2, 23),
-					Minutes = 0,
-					Id = Guid.NewGuid(),
-					IsPublic = false,
-					Status = true,
-					CustomMessage = "Custom msg: " + Guid.NewGuid().ToString(),
-				};
-				_dbContext.Add(eventCounter);
 
-				for (var j = 0; j < 5;  j++)
+			var baseUser = _dbContext.Users.Include(i => i.PersonalProfiles).FirstOrDefault();
+			var userQty = 50;
+
+			for (int currentUser = 0; currentUser < userQty; currentUser++)
+			{
+				var user = new User()
 				{
-					var relapse = new Relapse()
+					Id = Guid.NewGuid(),
+					AllowSysAdminAccess = true,
+					CreationDate = DateTime.Now,
+					Email = "test@mail.com",
+					Salt = baseUser.Salt,
+					PasswordHash = baseUser.PasswordHash,
+					UserName = "rolando_" + (_dbContext.Users.Count()),
+
+				};
+
+				_dbContext.Add(user);
+
+				var personalProfile = new PersonalProfile()
+				{
+					Address = "False street #123, Springfield, California, United States of America",
+					CreationDate = DateTime.Now,
+					LastName1 = "GenericLastName1",
+					LastName2 = "GenericLastName2",
+					Id = Guid.NewGuid(),
+					Name = "GenericName1",
+					Pohone = "812488583812",
+					UserId = user.Id,
+					DefaultPetPhotos = currentUser % 2 != 0 ? "" : Guid.NewGuid().ToString() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid()
+					+ Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid()
+					+ Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid()
+					+ Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid()
+					+ Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid()
+					+ Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid()
+					+ Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid()
+					+ Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid()
+					+ Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid()
+					+ Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid()
+					+ Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid()
+					+ Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid()
+					+ Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid()
+					+ Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid()
+					+ Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid()
+					+ Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid()
+					+ Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid()
+					+ Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid()
+					+ Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid()
+					+ Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid()
+					+ Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid()
+					+ Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid()
+					+ Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid()
+					+ Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid()
+					+ Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid()
+					+ Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid()
+				};
+
+				_dbContext.Add(personalProfile);
+
+				var anonimizedRequest = new RegisterModel()
+				{
+					Email = user.Email + Guid.NewGuid(),
+					LastName1 = personalProfile.LastName1,
+					LastName2 = personalProfile.LastName2,
+					Name = personalProfile.Name,
+					Password = "******************"
+				};
+
+				var requestAccount = new SignUpRequest()
+				{
+					CreationDate = DateTime.Now,
+					RequestObject = JsonSerializer.Serialize(anonimizedRequest),
+					Id = Guid.NewGuid(),
+					Ip = "192.168.0.112",
+					UserId = user.Id
+
+				};
+
+				_dbContext.Add(requestAccount);
+
+				for (int login = 0; login < 70; login++)
+				{
+					var newLogin = new CorrectLogin()
 					{
 						Id = Guid.NewGuid(),
-						CreationDate = DateTime.Now,
-						EventCounterId = eventCounter.Id,
-						PersonalProfileId = user.PersonalProfiles.First().Id,
-						UserId = user.Id,
-						PreviousDay = r.Next(1, 28),
-						PreviousMonth = r.Next(1, 12),
-						PreviousHour = r.Next(2, 12),
-						PreviousMinutes = 0,
-						RelapseDay = r.Next(1, 28),
-						RelapseMonth = r.Next(1, 12),
-						RelapseYear = r.Next(1990, 2030),
-						RelapseHour = r.Next(2, 23),
-						RelapseMinute = r.Next(1, 50),
-						PreviousYear = r.Next(1990, 2030)
+						IpAddress = "192.168.0.112",
+						LoginDate = DateTime.Now,
+						UserId = user.Id
 					};
-					_dbContext.Add(relapse);
+					_dbContext.Add(newLogin);
 				}
 
 				_dbContext.SaveChanges();
-			}
+				Random r = new Random();
+				for (var i = 0; i < 100; i++)
+				{
+					var eventCounter = new EventCounter()
+					{
+						UserId = user.Id,
+						PersonalProfileId = user.PersonalProfiles.First().Id,
+						CreationDate = DateTime.Now,
+						EventName = "Test  " + Guid.NewGuid(),
+						StartDay = r.Next(1, 28),
+						StartMonth = r.Next(1, 12),
+						StartYear = r.Next(1990, 2030),
+						Hour = r.Next(2, 23),
+						Minutes = 0,
+						Id = Guid.NewGuid(),
+						IsPublic = false,
+						Status = true,
+						CustomMessage = "Custom msg: " + Guid.NewGuid().ToString(),
+						CustomEventImageCollection = i %2 != 0 ? "" :  Guid.NewGuid().ToString() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid()
+						+ Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid()
+						+ Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid()
+						+ Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid()
+						+ Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid()
+						+ Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid()
+						+ Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid()
+						+ Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid()
+						+ Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid()
+						+ Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid()
+						+ Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid() + Guid.NewGuid()
+					};
+					_dbContext.Add(eventCounter);
 
+
+					if (i >= 25)
+						for (var j = 0; j < 2; j++)
+						{
+							var relapse = new Relapse()
+							{
+								Id = Guid.NewGuid(),
+								CreationDate = DateTime.Now,
+								EventCounterId = eventCounter.Id,
+								PersonalProfileId = user.PersonalProfiles.First().Id,
+								UserId = user.Id,
+								PreviousDay = r.Next(1, 28),
+								PreviousMonth = r.Next(1, 12),
+								PreviousHour = r.Next(2, 12),
+								PreviousMinutes = 0,
+								RelapseDay = r.Next(1, 28),
+								RelapseMonth = r.Next(1, 12),
+								RelapseYear = r.Next(1990, 2030),
+								RelapseHour = r.Next(2, 23),
+								RelapseMinute = r.Next(1, 50),
+								PreviousYear = r.Next(1990, 2030)
+							};
+							_dbContext.Add(relapse);
+						}
+
+					_dbContext.SaveChanges();
+				}
+
+			}
 		}
+
 	}
 }
