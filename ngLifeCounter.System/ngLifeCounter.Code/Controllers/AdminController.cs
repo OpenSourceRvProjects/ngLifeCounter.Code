3cda08891f8c638784c12d6cdd9a60365b82f917
dev-rvdl <dev-rvdl@outlook.es>
2024-04-06 18:47:45 -0600

    admin controller filter

--- ngLifeCounter.System/ngLifeCounter.Code/Controllers/AdminController.cs
+++ ngLifeCounter.System/ngLifeCounter.Code/Controllers/AdminController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using ngLifeCounter.Backend.Infrastructure;
+using ngLifeCounter.MVC.Filters;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -19,6 +20,7 @@ namespace ngLifeCounter.MVC.Controllers
         }
 
         [HttpGet]
+		[LoggedUserDataFilter]
 		[Route("getAllUsers")]
 		public async Task<IActionResult> Get()
 		{
